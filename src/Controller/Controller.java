d491dd3d2e24372ff7b192cd6c5dfb1698fd1caf
PettHub <aliver.ondersson@gmail.com>
2020-09-03 14:09:32 +0200

    hipp

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -6,20 +6,19 @@ import java.awt.event.KeyAdapter;
 
 
 import java.awt.event.KeyEvent;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class Controller extends KeyAdapter {
     Engine engine;
-    Map<Integer, Boolean> pressed = new HashMap<>();
 
+    public Controller(Engine engine) {
+        this.engine = engine;
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
         super.keyPressed(e);
     }
-
     //Dessa ska fyllas i med något vettigt, antagligen ett switch/case
     @Override
     public void keyReleased(KeyEvent e) {
