79eaf6bb8d484abc47ac204a3e56267194651713
PettHub <aliver.ondersson@gmail.com>
2020-09-08 10:31:03 +0200

    Update Calendar.java

--- src/Model/Calendar.java
+++ src/Model/Calendar.java
@@ -3,9 +3,11 @@ package Model;
 import java.util.List;
 
 public class Calendar {
+    java.util.Calendar calendar;
     boolean initialized = false;
     List<Date> dates;
     public void init(){
+        calendar = java.util.Calendar.getInstance();
         
     }
 }
