fe4dc4be15b6432eebe07065d8df65e8d80f9e3e
Ronserro <33457400+Ronserro@users.noreply.github.com>
2020-09-11 15:00:35 +0200

    Revert "Merge remote-tracking branch 'upstream/master'"
    This reverts commit e2cad7ad8ace6958b5460657ad05a4400c335a6e, reversing
    changes made to ab530e022f208b57e3f1eb4fabb6cdeee1079d89.

--- src/Model/WorkShift.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-public class WorkShift extends OccupiedTime {
-    final public int requiredPersonnel;
-    public WorkShift(long start, long end, int requiredPersonnel) {
-        super(start, end);
-        this.requiredPersonnel = requiredPersonnel;
-    }
-}
