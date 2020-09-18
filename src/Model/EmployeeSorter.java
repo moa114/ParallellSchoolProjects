c6a9b5cc0d1dfabc3280e7d248772420a2f3883d
PettHub <aliver.ondersson@gmail.com>
2020-09-18 14:36:15 +0200

    Update EmployeeSorter.java

--- src/Model/EmployeeSorter.java
+++ src/Model/EmployeeSorter.java
@@ -6,9 +6,10 @@ import java.util.List;
 public class EmployeeSorter {
     public List<Employee> getAvailablePersonnel(List<Employee> employees, List<String> certificates, long start, long stop) {
         List<Employee> newList = new ArrayList<>();
-        for (Employee e : employees)
-            if (e.certificates.containsAll(certificates))
+        for (Employee e : employees) {
+            if (e.certificates.containsAll(certificates) && !e.isOccupied(start, stop))
                 newList.add(e);
-            for()
+        }
+        return newList;
     }
 }
