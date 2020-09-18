62dd1054c9ab2be20823a318d37b4d0acf359ac6
PettHub <aliver.ondersson@gmail.com>
2020-09-18 14:34:31 +0200

    så

--- src/Model/EmployeeSorter.java
+++ src/Model/EmployeeSorter.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class EmployeeSorter {
     public List<Employee> getAvailablePersonnel(List<Employee> employees, List<String> certificates, long start, long stop) {
         List<Employee> newList = new ArrayList<>();
-        for ()
+        for (Employee e : employees)
+            if (e.certificates.containsAll(certificates))
+                newList.add(e);
+            for()
     }
 }
