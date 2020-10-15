1bf3904a48fd33208b78e26fa60853202b6967b5
TheThingIs <33488191+TheThingIs@users.noreply.github.com>
2020-10-15 18:18:08 +0200

    Observable Departments
    Implemented the observer pattern between workdays and workshifts
    workday.setWorkDay is no longer needed

--- src/Model/Department.java
+++ src/Model/Department.java
@@ -1,18 +1,22 @@
 package Model;
 
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
 /**
  * Represents a department with a specified name and a list for work shifts where the department can be manned. It also has a minimum value of persons that has to work at the same time
  */
-public class Department {
+public class Department implements Observable{
 
     private List<WorkShift> allShifts;
+    private List<WorkShift> addedShifts;
+    private List<WorkShift> removedShifts;
+    private List<Observer> observers;
     private String name;
     private int minPersonsOnShift;
     private BreakHandler breakHandler;
@@ -29,6 +33,12 @@ public class Department {
         this.name = name;
         this.minPersonsOnShift = minPersonsOnShift;
         this.breakHandler = BreakHandler.getInstance();
+        this.observers = new ArrayList();
+        this.addedShifts = new ArrayList();
+        this.removedShifts = new ArrayList();
+        for (int i = 0 ; i < OurCalendar.getInstance().getOurDateSize() ; i++){
+            addObserver(OurCalendar.getInstance().getWorkday(i));
+        }
     }
 
     public Color getColor(){
@@ -97,22 +107,31 @@ public class Department {
     protected void createShift(long start, long stop, List<Certificate> certificates, boolean[] repeat) {
         WorkShift ws = new WorkShift(start, stop, certificates, createBreak(start, stop), true);
         if (setRepeat(ws, repeat)) {
-            allShifts.add(new WorkShift(start, stop, certificates, createBreak(start, stop), false));
+            WorkShift shift = new WorkShift(start, stop, certificates, createBreak(start, stop), false);
+            allShifts.add(shift);
+            addedShifts.add(shift);
         }
+        notifyObservers();
     }
 
     protected void createShift(long start, long stop, Certificate certificate, boolean[] repeat) {
         WorkShift ws = new WorkShift(start, stop, certificate, createBreak(start, stop), true);
         if (setRepeat(ws, repeat)) {
-            allShifts.add(new WorkShift(start, stop, certificate, createBreak(start, stop), false));
+            WorkShift shift = new WorkShift(start, stop, certificate, createBreak(start, stop), false);
+            allShifts.add(shift);
+            addedShifts.add(shift);
         }
+        notifyObservers();
     }
 
     protected void createShift(long start, long stop, boolean[] repeat) {
         WorkShift ws = new WorkShift(start, stop, createBreak(start, stop), true);
         if (setRepeat(ws, repeat)) {
-            allShifts.add(new WorkShift(start, stop, createBreak(start, stop), false));
+            WorkShift shift = new WorkShift(start, stop, createBreak(start, stop), false);
+            allShifts.add(shift);
+            addedShifts.add(shift);
         }
+        notifyObservers();
     }
 
     protected void createShift(WorkShift ws) {
@@ -120,7 +139,9 @@ public class Department {
     }
 
     protected void createShift(WorkShift ws, int i) {
-        allShifts.add(new WorkShift(ws, i));
+        WorkShift shift = new WorkShift(ws, createBreak(ws.START + WeekHandler.plusDays(i), ws.END + WeekHandler.plusDays(i)), i);
+        allShifts.add(shift);
+        addedShifts.add(shift);
     }
 
     private boolean setRepeat(WorkShift ws, boolean[] repeat) {
@@ -137,6 +158,8 @@ public class Department {
 
     protected void removeShift(WorkShift ws) {
         allShifts.remove(ws);
+        removedShifts.add(ws);
+        notifyObservers();
     }
 
     public WorkShift getShift(int index){
@@ -175,4 +198,41 @@ public class Department {
     public void setName(String name) {
         this.name = name;
     }
+
+    public WorkShift getAddWorkShift(int index){
+        return addedShifts.get(index);
+    }
+
+    public int getAddWorkShiftSize(){
+        return addedShifts.size();
+    }
+
+    public WorkShift getRemoveWorkShift(int index) {
+        return removedShifts.get(index);
+    }
+
+    public int getRemoveWorkShiftSize(){
+        return removedShifts.size();
+    }
+
+    @Override
+    public void addObserver(Observer observer) {
+            observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for(Observer observer : observers) {
+            observer.update();
+        }
+        addedShifts.clear();
+        removedShifts.clear();
+    }
+
+
 }
